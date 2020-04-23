fa93c11dac09440e7163d0dd94b9484e175e4267
Ryan Stark <rstark@orbitform.com>
2020-04-23 18:23:51 -0400

    Cleanup; implement proxy handler; add main event / command loop

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import argparse
-import os.path
-
-
 # Version Check
 from platform import python_version
 if python_version()[0] != '3':
-    print("Must be run with python 3")
-    exit()
+	print("Must be run with python 3")
+	exit()
+
+
+
+# Imports
+import argparse
+import time
+import proxy_list
 
 
 
@@ -16,19 +19,33 @@ parser = argparse.ArgumentParser(description='''
 	PHlood 2020-05-09
 	CIS 210 2020
 	Morgan Matchette & Ryan Stark''')
-parser.add_argument('--logfile',
-                    help='File to log to',
-                    action='store',
-                    dest='logfile',
-                    type=str,
-                    required=False,
-                    metavar='FILE')
 argument = parser.parse_args()
 
 
 
-# File existance sanity check
-if os.path.isfile(argument.logfile):
-    overwrite = input("File exists. Overwrite (y/N)? ")
-    if len(overwrite) <= 0 or overwrite[0] not in ['Y', 'y']:
-        exit()
+proxy = proxy_list.proxy_list()
+proxy.add_source("https://free-proxy-list.net",		"#proxylisttable tbody tr",	"td:nth-child(1)",	"td:nth-child(2)",	"td:nth-child(7)",	{'no':'http://','yes':'https://'}	)
+#proxy.add_source("https://us-proxy.org",			"",	"",	""	)
+#proxy.add_source("https://hidemy.name/proxy-list",	"",	"",	""	)
+#proxy.add_source("https://socks-proxy.net",			"",	"",	""	)
+#proxy.add_source("https://sslproxies.org",			"",	"",	""	)
+proxy.start()
+
+
+
+while True:
+	command = input("\n\n===================\nAvailable Commands:\n" +
+					"\tExit\tStops all threads and exits program\n" +
+					"\tStatus\tDisplays quick status\n"
+					">>").split()
+	if len(command) == 0:
+		continue
+	if command[0].strip().lower() == 'exit':
+		break
+	if command[0].strip().lower() == 'status':
+		print("Proxies: " + str(len(proxy.list)))
+
+
+
+print("Exiting...")
+proxy.stop()
