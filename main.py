293a1cf874ac013e6d995e6581ad59122cc93cd7
Ryan Stark <rstark@orbitform.com>
2020-04-24 08:36:12 -0400

    Add credential generator

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if python_version()[0] != '3':
 import argparse
 import time
 import proxy_list
+import random
 
 
 
@@ -34,6 +35,47 @@ proxy.add_source("https://free-proxy-list.net",		"#proxylisttable tbody tr",	"td
 #proxy.add_source("https://socks-proxy.net",			"",	"",	""	)
 #proxy.add_source("https://sslproxies.org",			"",	"",	""	)
 proxy.start()
+
+
+
+with open('names.txt', 'r') as name_list:
+	names = name_list.read().splitlines()
+with open('passwords.txt', 'r') as password_list:
+	passwords = password_list.read().splitlines()
+with open('domains.txt', 'r') as domain_list:
+	domains = domain_list.read().splitlines()
+
+
+
+def generate_credentials():
+	domain = random.choice(domains)
+	choices = [
+		domain,
+		random.choice(domains),
+		random.choice(names),
+		random.choice(names),
+		random.choice(names),
+		random.choice(names),
+		random.choice(passwords)
+	]
+	email = ''
+	while len(email) < 6: # TODO : further validation
+		email = ''
+		while random.randint(0, 2):
+			choice = random.choice(choices)
+			start = random.randint(0, len(choice))
+			stop = random.randint(start, len(choice))
+			email = email + choice[start:stop]
+	email = email.lower() + '@' + domain.lower()
+	password = ''
+	while len(password) < 8: # TODO : further validation
+		password = ''
+		while random.randint(0, 4):
+			choice = random.choice(choices)
+			start = random.randint(0, len(choice))
+			stop = random.randint(start, len(choice))
+			password = password + choice[start:stop]
+	return (email, password)
 
 
 
